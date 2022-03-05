0a62791bae5ebbd14cf277776d44cd8e20859100
M@x <zmae@mail.ru>
2022-03-05 16:48:23 +0300

    Lesson 3. Django ORM start

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -7,5 +9,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", mainapp.main, name="main"),
     path("products/", mainapp.products, name="products"),
+    path("products/all", mainapp.products, name="products_all"),
+    path("products/home", mainapp.products, name="products_home"),
+    path("products/office", mainapp.products, name="products_office"),
+    path("products/modern", mainapp.products, name="products_modern"),
+    path("products/classic", mainapp.products, name="products_classic"),
     path("contact/", mainapp.contact, name="contact"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
