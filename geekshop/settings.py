79bb156a7f9eba536b06c39485492594954eb674
mzel-creator <82381351+mzel-creator@users.noreply.github.com>
2022-03-24 11:04:40 +0300

    Lasson 4 (#4)
    * Lesson 4. Lesson ready

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp",
+    "authnapp",
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Auth model
+AUTH_USER_MODEL = "authnapp.ShopUser"
